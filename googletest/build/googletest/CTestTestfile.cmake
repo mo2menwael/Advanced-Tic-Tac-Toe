fd1df24cd05d790eedbb12005f8ad2e620dcf862
Moamen Wael <moamenwael6@gmail.com>
2024-06-25 17:48:19 +0300

    Add build folder to googletest

---
+++ googletest/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest/googletest
+# Build directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
