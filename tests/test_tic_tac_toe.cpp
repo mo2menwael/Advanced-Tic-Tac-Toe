0eaef5707832ad32126d2a2efa77417321843751
Moamen Wael <moamenwael6@gmail.com>
2024-06-25 13:53:37 +0300

    Update test_tic_tac_toe.cpp

--- tests/test_tic_tac_toe.cpp
+++ tests/test_tic_tac_toe.cpp
@@ -1,4 +1,4 @@
-/*#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 #include "test_tic_tac_toe.h"
 
 // Test case for checking the initialization of the board
@@ -163,4 +163,3 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-*/
