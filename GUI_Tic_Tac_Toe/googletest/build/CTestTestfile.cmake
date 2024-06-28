873665ecd93b9dd76f5f07a0253efe965f07f286
Moamen Wael <moamenwael6@gmail.com>
2024-06-28 12:05:30 +0300

    Delete GUI_Tic_Tac_Toe/googletest/build directory

--- GUI_Tic_Tac_Toe/googletest/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest
-# Build directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
