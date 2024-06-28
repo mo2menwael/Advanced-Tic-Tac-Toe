96a9fa90dab88ece0ed031045fae0397cc2a63dc
mo2menwael <moamenwael6@gmail.com>
2024-06-28 11:52:07 +0300

    Rearrange folders

--- googletest/build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest/googletest
-# Build directory: C:/Users/moame/Documents/QT/Advanced-Tic-Tac-Toe/googletest/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
